d4fe028b546df9f76e60ef082e4ffbd1e743c7c2
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-11-30 11:33:37 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json){
+      let images = json.message
+      images.forEach((item) => {
+        let img = document.createElement('img')
+        img.src = item
+        document.querySelector('#dog-image-container').appendChild(img)
+      })
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(function (response){
+      return response.json()
+    })
+    .then(function (json){
+      let breeds = json.message
+      for (breed in breeds){
+        let li = document.createElement('li')
+        li.textContent = breed
+        document.querySelector('#dog-breeds').appendChild(li)
+      }
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  let breeds = document.querySelector('#dog-breeds')
+  breeds.addEventListener('click', function(e){
+    e.target.style.color = "red"
+  })
+})
+
